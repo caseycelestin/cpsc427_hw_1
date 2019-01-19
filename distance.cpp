df7439573745ca59257b8376ed45150b2f769642
Casey Celestin <caseycelestin@Caseys-MacBook-Pro-2.local>
2019-01-19 18:20:10 -0500

    Created distance.cpp, tested reading from stdin by line

---
+++ distance.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::string;
+using std::endl;
+
+int main(int argc, char **argv)
+{
+	//double lat1, lon1, lat2, lon2;
+	//double deg, min, sec;
+	string line;
+
+	getline(cin, line);
+
+	cout << line << endl;		
+}
