711722b5570f44fc3cc8024c5642742d3926bd71
Casey Celestin <caseycelestin@Caseys-MacBook-Pro-2.local>
2019-01-23 12:10:24 -0500

    Created geocoord.hpp for Coordinate class

---
+++ geocoord.hpp
@@ -0,0 +1,43 @@
+#ifndef __GEOCOORD_HPP__
+#define __GEOCOORD_HPP__
+
+namespace geocoord
+{
+	class Coordinate
+	{
+	public:
+		/**
+		* Initializes coordinate at longitude 0.0 deg, latitude 0.0 deg
+		*/
+		Coodinate();
+		
+		/**
+		* Initializes coordinate at longitude x, latitude y
+		* Arguments  are strings to handle Deg:Min:Sec form
+		*/
+		Coordinate(string, string);
+		
+		/**
+		* Computes the distance of two coordinates on earth given longitude and latitute of two points
+		*/
+		double geo_distance(Coordinate, Coordinate);
+
+	private:
+		/**
+		* Latitude of coordinate
+		*/
+		double lat;
+
+		/**
+		* Longitude of coordinate
+		*/
+		double lon;
+		
+		/**
+		* Changes a singular coordinate from Deg:Min:Sec to decimal form
+		*/
+		double coordinate_converter(string coord_in);
+	};
+}
+
+#endif
