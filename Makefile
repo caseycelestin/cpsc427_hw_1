af318d9cadd6f357a01ad82bad5a79b1b99d6cf5
Casey Celestin <caseycelestin@Caseys-MacBook-Pro-2.local>
2019-01-24 12:33:30 -0500

    Fixed Makefile to integrate coordinate class successfully

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 CXX=g++
-CXXFLAGS= -g -Wall -pedantic -std=c++17
+CXXFLAGS=-Wall -pedantic -std=c++17
 
 Distance: distance.o geocoord.o
-	${CXX} ${CXXFLAGS} -o Distance distance.o
+	${CXX} ${CXXFLAGS} -o Distance distance.o geocoord.o
 
 distance.o: geocoord.hpp
 geocoord.o: geocoord.hpp 
--- distance.cpp
+++ distance.cpp
@@ -14,19 +14,16 @@ using std::endl;
 
 int main(int argc, char **argv)
 {
-	string line1, line2, line3, line4;
-	//coord point1, point2;
+	string line1, line2, line3, line4;	
 
 	getline(cin, line1);
 	getline(cin, line2);
 	getline(cin, line3);
-	getline(cin, line4);
-	
-	coord point1;
+	getline(cin, line4);	
 
-	//coord point1{line1, line2};
-	//coord point2{line3, line4};
+	coord point1{line1, line2};
+	coord point2{line3, line4};
 	
-	//cout << point1.geo_distance(point2) << endl;
+	cout << point1.geo_distance(point2) << endl;
 }
 
